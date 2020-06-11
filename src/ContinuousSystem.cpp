41a825ab7d2e1cb9ba3ec9bab5cd0f43a1b958bc
tedklin <tedklin@gmail.com>
2020-06-11 12:44:58 -0700

    clang format

--- src/ContinuousSystem.cpp
+++ src/ContinuousSystem.cpp
@@ -3,25 +3,25 @@
 #include <complex>
 #include "Eigen/Eigen"
 
-// in retrospect probably should've inlined the getters
-const Eigen::Matrix3d& ContinuousSystem::GetSystem() const {
-	return system_;
-}
+// in retrospect probably should've inlined the getters in the header
+const Eigen::Matrix3d& ContinuousSystem::GetSystem() const { return system_; }
 
-const Eigen::EigenSolver<Eigen::Matrix3d>::EigenvalueType& ContinuousSystem::GetEigenvalues() const {
-	return eigenvalues_;
+// also this type is a mess, gotta alias this
+const Eigen::EigenSolver<Eigen::Matrix3d>::EigenvalueType&
+ContinuousSystem::GetEigenvalues() const {
+  return eigenvalues_;
 }
 
 bool ContinuousSystem::IsStable() const {
-	for (unsigned row = 0; row < eigenvalues_.rows(); ++row) {
-		if (eigenvalues_(row, 0).real() > 0) {
-			return false;
-		}
+  for (unsigned row = 0; row < eigenvalues_.rows(); ++row) {
+    if (eigenvalues_(row, 0).real() > 0) {
+      return false;
+    }
 
-		// discrete stability implementation for fun
-		// if (std::abs(eigenvalues_(row, 0)) >= 1) {
-		// 	return false;
-		// }
-	}
-	return true;
+    // discrete stability implementation for fun
+    // if (std::abs(eigenvalues_(row, 0)) >= 1) {
+    // 	return false;
+    // }
+  }
+  return true;
 }
