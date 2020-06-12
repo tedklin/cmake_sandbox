dd137d6de3f064dfdc9a6614dfb0da5c9a146029
tedklin <tedklin@gmail.com>
2020-06-12 12:41:33 -0700

    travis config

--- tests/ContinuousSystemTest.cpp
+++ tests/ContinuousSystemTest.cpp
@@ -3,9 +3,7 @@
 #include "Eigen/Eigen"
 #include "gtest/gtest.h"
 
-TEST(ContinuousSystemTest, SanityCheck) {
-  ASSERT_EQ(20, 20);
-}
+TEST(ContinuousSystemTest, SanityCheck) { ASSERT_EQ(20, 20); }
 
 TEST(ContinuousSystemTest, SaltoStable) {
   Eigen::Matrix3d A;
