d3686b8e4846791e121d63f12d33bc02c61a0134
tedklin <tedklin@gmail.com>
2020-06-12 15:11:00 -0700

    sublime header autocomplete workaround

--- include/cmake_sandbox/Core.h
+++ include/cmake_sandbox/Core.h
@@ -2,12 +2,12 @@
 
 // common includes across header files
 
-// dirty solution to the fact that my local dev setup autocomplete doesn't
+// dirty solution to the fact that EasyClangComplete for Sublime doesn't
 // recognize external libraries in .h files (.cpp works fine though).
 
 // for guaranteed CMake build success across systems, use "Eigen/Eigen".
-#include "Eigen/Eigen"
+// #include "Eigen/Eigen"
 
 // this references my local install of Eigen (/usr/local/include/eigen3),
-// purely for the purpose of my autocomplete.
-// #include "eigen3/Eigen/Eigen"
+// purely for the purpose of autocomplete.
+#include "eigen3/Eigen/Eigen"
